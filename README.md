276bd708d24371691f4406e21b6384fb40dcb86f
root <root@FQ-202112231216.localdomain>
2024-05-26 10:43:53 +0800

    add source code

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-This code is written with Arduino for an ESP8266 microcontroller to read temperature and humidity data from a DHT11 sensor and publish it to an MQTT server.
+This code is written with Arduino for an ESP8266 microcontroller to read temperature and humidity data from a DHT11 sensor and publish it to an MQTT server. And control a fan to lower the temperature if it is higher than 40 degree by default.
 
  I am using this program to get my personal rack environment information and present with homeassistant dashboard.
+
+ Please be aware that although the relay I'm using is rated for 5V, I've discovered that it requires connection to a 3.3V power source at the VCC pin for proper operation. Connecting it to a 5V source doesn't seem to work. In addition, set the relay to low level trigger is also required. 
--- rackEnv.ino
+++ rackEnv.ino
@@ -2,74 +2,104 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
-int pinDHT11 = 2; // GPIO2 of ESP8266
-const char* ssid = "qi";//wifi ssid
-const char* password = "xxxxxxx";//wifi password
-const char* mqtt_server = "192.168.50.102";//mqtt server address
+#define RELAY_PIN 0
+#define DHT11_PIN 2
+const char *ssid = "qi";                    // wifi ssid
+const char *password = "xxxxxxx";           // wifi password
+const char *mqtt_server = "192.168.50.102"; // mqtt server address
 
-//const String macAddress = WiFi.macAddress();
-//const char* clientID = macAddress.c_str();    //MAC address
-const char* clientID = "rackenv_dht11";
+// const String macAddress = WiFi.macAddress();
+// const char* clientID = macAddress.c_str();    //MAC address
+const char *clientID = "rackenv_dht11";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
-SimpleDHT11 dht11(pinDHT11);
+SimpleDHT11 dht11(DHT11_PIN);
 
 long lastMsg = 0;
 char msg[50];
 int value = 0;
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
+  pinMode(RELAY_PIN, OUTPUT); // Initialize the relay pin as an output
+  //Open the relay in the beginning, assuming HIGH opens the relay
+  digitalWrite(RELAY_PIN, HIGH); 
 }
 
-void setup_wifi() {
+void setup_wifi()
+{
 
   delay(10);
-  WiFi.begin(ssid, password);    
-  while (WiFi.status() != WL_CONNECTED) {
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
   }
   Serial.println("WiFi connected");
 }
 
-void callback(char* topic, byte* payload, unsigned int length) {
- Serial.println("Message arrived ["); Serial.print(topic);   Serial.print("] ");
-  for (int i = 0; i < length; i++) {
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  Serial.println("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++)
+  {
     Serial.println((char)payload[i]);
-  }  
+  }
 }
 
-void reconnect() {
-  while (!client.connected()) {
+void reconnect()
+{
+  while (!client.connected())
+  {
     Serial.println("reconnecting mqtt...");
     delay(1000);
-    if (client.connect(clientID,"admin","mengqi2")) {               //connected
-      Serial.print("MQTT connected!");                
-    } else {
-      delay(5000);                                
+    if (client.connect(clientID, "admin", "mengqi2"))
+    { // connected
+      Serial.print("MQTT connected!");
+    }
+    else
+    {
+      delay(5000);
     }
   }
 }
-void loop() {
+void loop()
+{
   Serial.println("start loop");
-  if (!client.connected()) {
+  if (!client.connected())
+  {
     reconnect();
   }
   client.loop();
   byte temperature = 0;
   byte humidity = 0;
   int err = SimpleDHTErrSuccess;
-  if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+  if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess)
+  {
     Serial.println("Read DHT11 failed, err=");
     Serial.println(err);
     delay(1000);
-  } else {
-      client.publish("rack/temperature", String(temperature).c_str());  //publish temperature
-      client.publish("rack/humidity", String(humidity).c_str());        //publish humidity
-      delay(3600000);
+  }
+  else
+  {
+    client.publish("rack/temperature", String(temperature).c_str()); // publish temperature
+    client.publish("rack/humidity", String(humidity).c_str());       // publish humidity
+    //  Close the relay if the temperature is greater than 30
+    if (temperature > 40)
+    {
+      digitalWrite(RELAY_PIN, LOW); // Assuming HIGH closes the relay
+    }
+    else if (temperature < 35) //To avoid frequent switching of the relay
+    {
+      digitalWrite(RELAY_PIN, HIGH); // Assuming LOW opens the relay
+    }
+    delay(3600000);
   }
 }
